17024a8b3c996f00cdcb448df45c97a5e05cf67a
ktsin <43731609+ktsin@users.noreply.github.com>
2021-05-04 08:32:35 +0300

    model v(0)

--- main_app/models.py
+++ main_app/models.py
@@ -3,3 +3,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class Good(models.Model):
+    name=models.CharField(max_length=511)
+    unit=models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Price')
+
+
+class GoodIncome(models.Model):
+    good=models.ForeignKey(Good, on_delete=models.CASCADE)
+    date=models.DateField()
+    checkout_number=models.IntegerField()
+
+
+class SystemUser(User):
+    isWorker=models.BooleanField(default=False)

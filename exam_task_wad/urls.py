9b867457770b6661d7253d63b8400c09079f6873
ktsin <43731609+ktsin@users.noreply.github.com>
2021-05-04 07:48:42 +0300

    setted up layout

--- exam_task_wad/urls.py
+++ exam_task_wad/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import main_app.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('layout/', main_app.views.layout_example)
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def layout_example(request):
+    return render(request, 'layout.html')
